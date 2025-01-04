f880e3c443c42da5f5d727f462a1cd3059476627
egekoybasi9 <egekoybasi931@gmail.com>
2025-01-04 09:15:54 +0100

    Update [[path]].js

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,7 +1,7 @@
 export async function onRequest({ request }) {
 	const url = new URL(request.url)
 
-	const proxyUrl = 'https://ioeorptes.world'
+	const proxyUrl = 'https://gotolopreps.world'
 	const backendUrl = `${proxyUrl}${url.pathname}`
 
 	const headers = new Headers(request.headers)
